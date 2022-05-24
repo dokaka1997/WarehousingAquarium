7ef2569e5926afd761489682f5de4e0c161167f8
dodv <dodv@smartosc.com>
2022-05-24 23:18:01 +0700

    update api product batch

--- WarehousingAquarium/src/main/java/com/warehousing/aquarium/controller/ProductBatchController.java
+++ WarehousingAquarium/src/main/java/com/warehousing/aquarium/controller/ProductBatchController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin
-@RequestMapping("warehouse")
+@RequestMapping("product_batch")
 public class ProductBatchController {
 
     ProductBatchService productBatchService;
